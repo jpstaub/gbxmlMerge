26fbb7180965fbb96b431adfc0f1dfc367224c0f
jpstaub <jpstaub1@gmail.com>
2022-05-30 14:19:21 -0400

    Topologic development
    gbxmlMerge based on Topologic geometry library.

--- topologic/faceByVertices/Topologic_faceByVertices_27MAY.py
+++ topologic/faceByVertices/Topologic_faceByVertices_27MAY.py
@@ -15,13 +15,17 @@ def faceByVertices(vertices):
 
     v1 = vertices[-1]
     v2 = vertices[0]
+    print("V1:", v1.X(), v1.Y(), v1.Z())
+    print("V2:", v2.X(), v2.Y(), v2.Z())
     try:
         e = topologic.Edge.ByStartVertexEndVertex(v1, v2)
         if e:
             edges.append(e)
     except:
+        print("Edge creation failed!")
         pass
-    if len(edges) > 3:
+    print("I managed to create",len(edges),"edges")
+    if len(edges) >= 3:
         c = topologic.Cluster.ByTopologies(edges, False)
         w = c.SelfMerge()
         if w.Type() == topologic.Wire.Type() and w.IsClosed():
@@ -32,13 +36,12 @@ def faceByVertices(vertices):
         raise Exception("Error: could not get a valid number of edges")
     return f
 
-v1 = topologic.Vertex.ByCoordinates(0,0,0)
-v2 = topologic.Vertex.ByCoordinates(10,0,0)
-v3 = topologic.Vertex.ByCoordinates(10,10,0)
-v4 = topologic.Vertex.ByCoordinates(0,10,0)
-lv = [v1,v2,v3,v4]
-# f1 = faceByVertices([v1,v2,v3,v4])
-f1 = faceByVertices(lv)
+v1 = topologic.Vertex.ByCoordinates(80.7243728,112.7768145,0)
+v2 = topologic.Vertex.ByCoordinates(80.7243728,112.7768145,10.5)
+v3 = topologic.Vertex.ByCoordinates(80.7243728,124.2768145,10.5)
+v4 = topologic.Vertex.ByCoordinates(80.7243728,124.2768145,0)
+
+f1 = faceByVertices([v1,v2,v3,v4])
 print(f1)
 c1 = f1.Centroid()
 print("Centroid:", c1.X(), c1.Y(), c1.Z())
--- topologic/topologic merge.py
+++ topologic/topologic merge.py
@@ -5,76 +5,77 @@ Spyder Editor
 This is a temporary script file.
 """
 
-#Todo: implement Topologic (https://topologic.app/user_doc :: Topologic:Utilities:FaceUtility:IsInside) to detect containment of opening vertex in surface faces.
+# Todo: implement Topologic (https://topologic.app/user_doc :: Topologic:Utilities:FaceUtility:IsInside) to detect containment of opening vertex in surface faces.
+# Topologic on PyPi: https://test.pypi.org/project/topologicpy/
 
-# import packages
+
+# import: modules
 from lxml import etree
 from xgbxml import get_parser
 import matplotlib.pyplot as plt
 from copy import copy
-# import geometer as gm
-# import numpy as np
-import topologicpy as tp
+from topologicpy import topologic as tp # from 'foo' import 'bar': this syntax required for topologicpy functionality
 
 
-# file gui
+# import: gui modules
 from tkinter import ttk
-
 from tkinter import *
 from tkinter.ttk import *
 from tkinter import filedialog
+Tk().withdraw() # hides command line window
 
-Tk().withdraw()
 
-# define file variables
-fpa = "./Input/gbXML A_Geometry.xml"
-fpb = "./Input/gbXML B_Opening.xml"
-fpo = "./Output/gbxml C_GeometryOpening.xml"
+# # define: file variables: sandbox project
+# fpa = "./Input/01_Simplified/gbXML A_Geometry.xml"
+# fpb = "./Input/01_Simplified/gbXML B_Opening_Multiple.xml"
+# fpo = "./Output/01_Simplified/gbXML C_GeometryOpenings.xml"
 
-# # define file variables with gui
-# fpa = filedialog.askopenfilename(title="gbXML Without Openings", filetypes=[("xml","*.xml")])
-# fpb = filedialog.askopenfilename(title="gbXML With Openings", filetypes=[("xml","*.xml")])
-# fpo = filedialog.asksaveasfilename(defaultextension='.xml', filetypes=[("xml","*.xml")])
+# # define: file variables: production project
+# fpa = "./Input/02_Production/22-013 Blue Star Kilbourne_Geometry.xml"
+# fpb = "./Input/02_Production/22-013 Blue Star Kilbourne_Openings.xml"
+# fpo = "./Output/02_Production/22-013 Blue Star Kilbourne_GeometryOpenings.xml"
 
-# uses xgbxml to generate a lxml parser to read gxXML version 0.37
+
+# define: file variables with gui
+fpa = filedialog.askopenfilename(title="gbXML Without Openings", filetypes=[("xml","*.xml")])
+fpb = filedialog.askopenfilename(title="gbXML With Openings", filetypes=[("xml","*.xml")])
+fpo = filedialog.asksaveasfilename(defaultextension='.xml', filetypes=[("xml","*.xml")])
+
+
+# use: xgbxml to generate a lxml parser / read: gxXML version 0.37
 parser=get_parser(version='0.37')
 
-# opens the file using the lxml parser
+
+# open: the file using the lxml parser
 tree_A = etree.parse(fpa,parser)
 gbxml_A = tree_A.getroot()
 
-# # renders the gbXML etree
+
+# # render: the gbXML etree
 # ax = gbxml_A.Campus.render()
 # ax.figure.set_size_inches(8, 8)
 # ax.set_title('gbXML A_Geometry.xml')
 # plt.show()
 
-# opens the file using the lxml parser
+
+# open: the file using the lxml parser
 tree_B = etree.parse(fpb,parser)
 gbxml_B = tree_B.getroot()
 
-# # renders the gbXML etree
+
+# # render: the gbXML etree
 # ax = gbxml_B.Campus.render()
 # ax.figure.set_size_inches(8, 8)
 # ax.set_title('gbXML B_Openings.xml')
 # plt.show()
 
-# creates a copy of gbxml_A which is named gbxml_C
+
+# make: a copy of gbxml_A which is named gbxml_C
 gbxml_C = copy(gbxml_A)
 etree_C = etree.ElementTree(gbxml_C)
 
-# topologicpy functional development & test sandbox
-# dpt = (86.2243728, 124.2768145, 3.0)
-# vert = tp.topologic.Vertex.ByCoordinates(dpt[0],dpt[1],dpt[2])
-# coord = tp.topologic.Vertex.Coordinates(vert)
 
-# generate opening vertices (ov)
-# l = len(gbxml_B.Campus.Surfaces.Openings)
-ov = []
-for opening in gbxml_B.Campus.Surfaces.Openings:
-    opt = opening.PlanarGeometry.PolyLoop.CartesianPoint.get_coordinates()
-    ov.append(tp.topologic.Vertex.ByCoordinates(opt[0],opt[1],opt[2]))
-    
+# define: topologicpy faceByVertices (Wassim Jabi) - 27MAY
 def faceByVertices(vertices):
     vertices
     edges = []
@@ -90,13 +91,17 @@ def faceByVertices(vertices):
 
     v1 = vertices[-1]
     v2 = vertices[0]
+    # print("V1:", v1.X(), v1.Y(), v1.Z())
+    # print("V2:", v2.X(), v2.Y(), v2.Z())
     try:
         e = tp.Edge.ByStartVertexEndVertex(v1, v2)
         if e:
             edges.append(e)
     except:
+        print("Edge creation failed!")
         pass
-    if len(edges) > 3:
+    # print("I managed to create",len(edges),"edges")
+    if len(edges) >= 3:
         c = tp.Cluster.ByTopologies(edges, False)
         w = c.SelfMerge()
         if w.Type() == tp.Wire.Type() and w.IsClosed():
@@ -106,59 +111,69 @@ def faceByVertices(vertices):
     else:
         raise Exception("Error: could not get a valid number of edges")
     return f
+
     
-# generate surface faces (sf)
-sf = []
-for surface in gbxml_C.Campus.Surfaces:
-    if surface.get_attribute('surfaceType') == 'ExteriorWall':
-        sv = []
-        # s = surface.PlanarGeometry.get_coordinates()
-        for spt in surface.PlanarGeometry.get_coordinates():
-            sv.append(tp.topologic.Vertex.ByCoordinates(spt[0],spt[1],spt[2]))
-        print(sv)
-        sf.append(faceByVertices(sv))
+# make: opening centroids (ocs)
+ocs = []
+for op in gbxml_B.Campus.Surfaces.Openings:
+    o = []
+    for c in op.PlanarGeometry.get_coordinates():
+        o.append(tp.Vertex.ByCoordinates(c[0],c[1],c[2]))
+    ocs.append(tp.Topology.Centroid(faceByVertices(o)))
 
-# # find openings in gbxml_B and merge with gbxml_C (Original - fail: counts down openings and prevents cycle of all openings)
-# openings = list(gbxml_B.Campus.Surfaces.Openings)
-# for su in gbxml_C.Campus.Surfaces:
-#     for opening in openings:
-#         try:
-#             su.copy_opening(opening,tolerance=0.01)
-#             openings.remove(opening)
-#             break
-#         except ValueError:
-#             pass
 
-# find openings in gbxml_B and merge with gbxml_C (Dev Attempt #1 - pass: cycles openings through all surfaces)
-for opening in gbxml_B.Campus.Surfaces.Openings:
-    for surface in gbxml_C.Campus.Surfaces:
-        if surface.get_attribute('surfaceType') == 'ExteriorWall':
-            try:
-                surface.copy_opening(opening,tolerance=0.01)
-                # openings.remove(opening)
-                break
-            except ValueError:
-                pass
-        
-# # find openings in gbxml_B and merge with gbxml_C (Dev Attempt #2 - fail: does not cycle surface more than once)
-# openings = list(gbxml_B.Campus.Surfaces.Openings)
-# for su in gbxml_C.Campus.Surfaces:
-#     for opening in openings:
-#         try:
-#             su.copy_opening(opening,tolerance=0.01)
-#             # openings.remove(opening)
-#             break
-#         except ValueError:
-#             pass        
-          
+# make: surface faces (sfs)
+sfs = []
+for su in gbxml_A.Campus.Surfaces:
+    # if surface.get_attribute('surfaceType') == 'ExteriorWall':
+        s = []
+        for c in su.PlanarGeometry.get_coordinates():
+            s.append(tp.Vertex.ByCoordinates(c[0],c[1],c[2]))
+        sfs.append(faceByVertices(s))
 
-# # renders the gbXML etree
-# ax = gbxml_C.Campus.render()
-# ax.figure.set_size_inches(8, 8)
-# ax.set_title('gbXML C_Composite.xml')
-# plt.show()
 
-# writes the gbXML_C etree to a local file
-# etree_C.write(fpo, pretty_print=True)
+# test: opening vertex IsInside(face,point,tolerance) of surface face (vin)
+vin = []
+for oc in ocs:
+    r = []
+    for sf in sfs:
+        r.append(tp.FaceUtility.IsInside(sf,oc,0.01))
+    vin.append(r)
+    
+    
+# # qa: count number of true responses per opening vertex
+# count = []
+# for v in vin:
+#     count.append(v.count(True))
+    
+    
+# get: indices of vertex isinside of surface face (ivif)
+ivif = []
+for v in vin:
+    ivif.append(v.index(True))
+
+
+# make: list of surface objects (suobs)
+suobs = []
+for i in ivif:
+    suobs.append(gbxml_C.Campus.Surfaces[i])
+    
+
+# insert: opening into gbxml_C surface
+i = 0
+for op in gbxml_B.Campus.Surfaces.Openings:
+    suobs[i].insert(3, op)
+    i = i+1
+     
+
+# render: the gbXML etree
+ax = gbxml_C.Campus.render()
+ax.figure.set_size_inches(8, 8)
+ax.set_title('gbXML C_Composite.xml')
+plt.show()
+
+
+# write: the gbXML_C etree to a local file
+etree_C.write(fpo, pretty_print=True)
 
 
